6da9223330c1fa3fcf573efc173030bc8336f14b
edisonjt <eddyt1999@gmail.com>
2023-06-22 23:03:33 -0500

    fix: :fire: Create components of elements in home

---
+++ src/pages/Home/components/Calendar.js
@@ -0,0 +1,36 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import React from 'react'
+import { Typography } from "@mui/material";
+
+const Calendar = () => {
+  return (
+    <div style={{ width: "50%", padding: "10px" }}>
+          <div
+            style={{
+              width: "100%",
+              textAlign: "center",
+              textTransform: "uppercase",
+              fontSize: "25px",
+            }}
+          >
+            <Typography variant="h5" marginBottom={10}>Calendario</Typography>
+          </div>
+          <div>
+            <div>
+              <FullCalendar
+                plugins={[dayGridPlugin]}
+                initialView="dayGridMonth"
+                events={[
+                  { title: "Event 1", date: "2023-06-01" },
+                  { title: "Event 2", date: "2023-06-05" },
+                  { title: "Event 3", date: "2023-06-10" },
+                ]}
+              />
+            </div>
+          </div>
+        </div>
+  )
+}
+
+export default Calendar

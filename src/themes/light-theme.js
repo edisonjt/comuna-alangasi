0a2f3a6377cb53f654f83d26f45e8c6f78813268
edisonjt <eddyt1999@gmail.com>
2023-06-22 23:03:03 -0500

    feat: :construction: Add light theme to application (in construction)

---
+++ src/themes/light-theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+export const lightTheme = createTheme({
+    typography:{
+        h6:{
+            fontFamily: 'sans-serif',
+            color:"#FF0000"
+        }
+        
+    }
+})
